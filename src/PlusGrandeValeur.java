2f65848dd8cbbe0934933efc1d918fda70cf71e6
Crepin21 <fouelefackcrepin@gmail.com>
2022-10-20 09:41:53 -0300

    Declaration des constantes

---
+++ src/PlusGrandeValeur.java
@@ -0,0 +1,22 @@
+/*
+  Titre      : PlusGrandeValeur
+  Auteur     : Crepin Vardin Fouelefack
+  Date       : 10/20/2022
+  Description: Remplissage dynamique d'un tableau et affichage du max et son index
+  Version    : 0.0.1
+*/
+
+//Importation des librairies necessaires
+import java.util.Random; 
+
+public class PlusGrandeValeur 
+{
+    public static void main(String[] args) throws Exception 
+    {
+
+        //Declaration des constantes
+        int MAX = 100;
+        int MIN = 0;
+        
+    }
+}
